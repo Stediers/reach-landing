4cd36cc9f8e3ab0cc053bff800646ffff72162ed
GAUTHUM.J <jgauthum@gmail.com>
2024-02-26 00:00:08 +0530

    .

--- app/api/mail_api/route.ts
+++ app/api/mail_api/route.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { sendMail } from "helper_functions/mailService";
 import { FormContent } from "@data/types";
+import { NextRequest, NextResponse as Response } from "next/server";
 
 // This is an example of how to send a server-side email
-export async function POST(req: NextApiRequest) {
+export async function POST(req: NextRequest) {
   //@ts-ignore
   const body = await req.json();
   try {
