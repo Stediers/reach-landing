30dc6893502134137e19bf20ace1fb255bb81670
Harshavardhan <68555960+Harshavardhanjo@users.noreply.github.com>
2024-06-15 00:10:19 +0530

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Reach Gig",
     short_name: "ReachGig",
     description: "gig app",
-    start_url: "/",
+    start_url: "/explore",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
