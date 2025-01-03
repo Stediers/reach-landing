1a7a0aea9e33014d3721a5931174c689eba3ff5c
Harshavardhan <134832073+reach4762@users.noreply.github.com>
2025-01-03 13:37:46 +0530

    Update layout.tsx

--- app/(landing)/vendors/layout.tsx
+++ app/(landing)/vendors/layout.tsx
@@ -5,7 +5,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="relative w-full px-5 lg:px-10 py-5 space-y-5 max-w-7xl">
       <TopBreadCrumb />
       {children}
-      {/* <SelectCity cities={["New York", "San Francisco", "Los Angeles"]} /> */}
     </div>
   );
 }
