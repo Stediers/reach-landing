6b3a5f1802a3d1e5742a25c6621cc05b5c9ec22b
Harshavardhan <134832073+reach4762@users.noreply.github.com>
2025-03-25 11:07:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: false,
   images: {
     unoptimized: true,
-    loader: "imgix",
     // remotePatterns: [
     //   "business-card-templates4762.s3.ap-south-1.amazonaws.com",
     //   "appointment-files4762.s3.ap-south-1.amazonaws.com",
